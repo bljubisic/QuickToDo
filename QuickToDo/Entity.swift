a2354cdf0d89e76f64bfa393f93de42227591fc2
bljubisic <bratislav.ljubisic@icloud.com>
2015-02-07 00:56:00 +0100

    Core Data model

---
+++ QuickToDo/Entity.swift
@@ -0,0 +1,20 @@
+//
+//  Entity.swift
+//  QuickToDo
+//
+//  Created by Bratislav Ljubisic on 10/19/14.
+//  Copyright (c) 2014 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Entity: NSManagedObject {
+
+    @NSManaged var completed: NSNumber
+    @NSManaged var count: NSNumber
+    @NSManaged var lastused: NSDate
+    @NSManaged var used: NSNumber
+    @NSManaged var word: String
+
+}
