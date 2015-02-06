4a4544955accf868d71f0bf8bd6287594697fbf2
bljubisic <bratislav.ljubisic@icloud.com>
2015-02-07 00:54:48 +0100

    View table cell

---
+++ QuickToDo/ViewTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ViewTableViewCell.swift
+//  QuickToDo
+//
+//  Created by Bratislav Ljubisic on 11/19/14.
+//  Copyright (c) 2014 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+
+class ViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var listItem: UILabel!
+    @IBOutlet weak var usedButton: UIButton!
+    
+}
