9e24e25cb915544fa42183916aa5e7941fed12e1
bljubisic <bratislav.ljubisic@icloud.com>
2015-03-11 23:56:40 +0100

    Added button action and app badge updates

--- QuickToDo/ViewController.swift
+++ QuickToDo/ViewController.swift
@@ -23,32 +23,72 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBAction func removeUsed(sender: UIButton) {
         dataManager.removeUsedItems()
         var deleteIndexPath: [NSIndexPath] = [NSIndexPath]()
-        println(self.items.count)
-        for(var i = 0; i < items.count; i++) {
-            let tmpItem: ItemObject = items[i]
+        //println(self.items.count)
+        var i = items.count
+        do {
+            let tmpItem: ItemObject = items[i-1]
             
             if(tmpItem.completed > 0) {
-                self.items.removeAtIndex(i)
-                println(self.items.count)
+                self.items.removeAtIndex(i-1)
+                //println(self.items.count)
                 deleteIndexPath.append(NSIndexPath(forRow: i, inSection: 0))
             }
-        }
+            i--
+        } while(i > 0)
         items = dataManager.getItems()
         itemsTable.deleteRowsAtIndexPaths(deleteIndexPath, withRowAnimation: UITableViewRowAnimation.Fade)
         itemsTable.reloadData()
     }
     
+    @IBAction func selectedItem(sender: AnyObject) {
+        /*
+        CGPoint buttonPosition = [sender convertPoint:CGPointZero toView:self.tableView];
+        NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:buttonPosition];
+        if (indexPath != nil)
+        {
+            ...
+        }
+        */
+        
+        var buttonPosition: CGPoint = sender.convertPoint(CGPoint.zeroPoint, toView: self.itemsTable)
+        var indexPath: NSIndexPath = self.itemsTable.indexPathForRowAtPoint(buttonPosition)!
+        
+        var row: Int = indexPath.row
+        
+        if(row <= items.count-1) {
+            var tmpCell: ViewTableViewCell = itemsTable.cellForRowAtIndexPath(indexPath) as ViewTableViewCell
+            var item: ItemObject = items[row]
+            if(item.completed == 1) {
+                item.completed = 0
+                tmpCell.usedButton.selected = false
+            } else {
+                item.completed = 1
+                tmpCell.usedButton.selected = true
+            }
+            self.itemsTable.reloadData()
+            dataManager.updateItem(item)
+            var usedItems: [ItemObject] = dataManager.getNotCompletedItems()
+            
+            UIApplication.sharedApplication().applicationIconBadgeNumber = usedItems.count
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dataManager.removeItems()
+        //dataManager.removeItems()
         
         items = dataManager.getItems()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        /* [[NSNotificationCenter defaultCenter] addObserver:self
+            selector:@selector(someMethod:)
+        name:UIApplicationDidBecomeActiveNotification object:nil]; */
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "applicationBecameActive:", name: UIApplicationDidBecomeActiveNotification, object: nil)
         
         let numberOfSections = self.itemsTable.numberOfSections()
         let numberOfRows = self.itemsTable.numberOfRowsInSection(numberOfSections-1)
@@ -62,6 +102,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var tblView =  UIView(frame: CGRectZero)
         self.itemsTable.tableFooterView = tblView
         self.itemsTable.backgroundColor = UIColor.clearColor()
+        
+        var usedItems: [ItemObject] = dataManager.getNotCompletedItems()
+        
+        UIApplication.sharedApplication().applicationIconBadgeNumber = usedItems.count
+
+    }
+    
+    func applicationBecameActive(notification: NSNotification) {
+        
+        var usedItems: [ItemObject] = dataManager.getNotCompletedItems()
+        
+        UIApplication.sharedApplication().applicationIconBadgeNumber = usedItems.count
+        
+        self.items = dataManager.getItems()
+        
+        self.itemsTable.reloadData()
     }
     
     func keyboardWillShow(notification: NSNotification) {
@@ -103,15 +159,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         if(row <= items.count-1) {
             var tmpCell: ViewTableViewCell = itemsTable.cellForRowAtIndexPath(indexPath) as ViewTableViewCell
             var item: ItemObject = items[row]
+            var badgeNumber: Int = UIApplication.sharedApplication().applicationIconBadgeNumber
             if(item.completed == 1) {
                 item.completed = 0
                 tmpCell.usedButton.selected = false
+                badgeNumber++
             } else {
                 item.completed = 1
                 tmpCell.usedButton.selected = true
+                badgeNumber--
             }
             self.itemsTable.reloadData()
             dataManager.updateItem(item)
+            UIApplication.sharedApplication().applicationIconBadgeNumber = badgeNumber
         }
     }
     
@@ -122,18 +182,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var row: Int = indexPath.row
         
         if(self.items.count == 0 || row > self.items.count - 1) {
-            var tmpCell: InputTableViewCell = tableView.dequeueReusableCellWithIdentifier("EnterCell") as InputTableViewCell
+            var tmpCell: InputTableViewCell = tableView.dequeueReusableCellWithIdentifier("EnterCell")as InputTableViewCell
             textView = tmpCell.inputTextField
             hintButton1 = tmpCell.addButton1
             hintButton2 = tmpCell.addButton2
-            hintButton1?.titleLabel?.text = ""
-            hintButton2?.titleLabel?.text = ""
-            textView?.backgroundColor = UIColor.orangeColor()
+            hintButton1?.setTitle("", forState: UIControlState.Normal)
+            hintButton2?.setTitle("", forState: UIControlState.Normal)
+            hintButton1?.enabled = false
+            hintButton2?.enabled = false
+            
+            hintButton1?.addTarget(self, action: "hintButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+            hintButton2?.addTarget(self, action: "hintButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+            //textView?.backgroundColor = UIColor.orangeColor()
+            textView?.layer.cornerRadius=8.0
+            textView?.layer.masksToBounds=true
+            textView?.layer.borderColor = UIColor.redColor().CGColor
+            
+            textView?.layer.borderWidth = 1.0
             
             textView?.addTarget(self, action: "textFieldDidChange:", forControlEvents: UIControlEvents.EditingChanged)
-            
-            
-             //   [textField addTarget:self action:@selector(textFieldDone:) forControlEvents:UIControlEventEditingDidEndOnExit];
             
             textView?.addTarget(self, action: "textFieldDone:", forControlEvents: UIControlEvents.EditingDidEndOnExit)
             textView?.delegate = self
@@ -143,7 +211,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         else {
             var tmpCell: ViewTableViewCell = tableView.dequeueReusableCellWithIdentifier("ViewCell") as ViewTableViewCell
-            tmpCell.listItem.text = self.items[row].word
+            tmpCell.listItem.text = self.items[row].word as String
             if(self.items[row].completed == 1) {
                 tmpCell.usedButton.selected = true
             }
@@ -156,17 +224,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    func hintButtonTapped(buttonTapped: UIButton) {
+        var text: String? = buttonTapped.titleLabel?.text
+        
+        textView?.text = text
+    }
+    
     func textFieldDidChange(textFieldSender: UITextField) {
+        
         var text: String = textFieldSender.text
-        var hints: [String] = dataManager.getHints(text)
+        var hints: [String] = [String]()
+        
+        if(text != "") {
+            hints = dataManager.getHints(text)
+        }
         
         if(hints.count > 1) {
-            hintButton1?.titleLabel?.text = hints[0]
-            hintButton2?.titleLabel?.text = hints[1]
-            
+            hintButton1?.setTitle(hints[0], forState: UIControlState.Normal)
+            hintButton2?.setTitle(hints[1], forState: UIControlState.Normal)
+            hintButton1?.enabled = true
+            hintButton2?.enabled = true
         }
-        else if(hints.count > 0) {
-            hintButton1?.titleLabel?.text = hints[0]
+        else if (hints.count == 1) {
+            hintButton1?.setTitle(hints[0], forState: UIControlState.Normal)
+            hintButton2?.setTitle("", forState: UIControlState.Normal)
+            hintButton1?.enabled = true
+        }
+        else if(hints.count == 0) {
+            hintButton1?.setTitle("", forState: UIControlState.Normal)
+            hintButton2?.setTitle("", forState: UIControlState.Normal)
+            hintButton1?.enabled = false
+            hintButton2?.enabled = false
         }
         
     }
@@ -176,7 +264,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         var itemObject: ItemObject = ItemObject()
         itemObject.word = item
-        itemObject.used = 0
+        itemObject.used = 1
         itemObject.count = 1
         itemObject.lasUsed = NSDate()
         
@@ -195,6 +283,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let indexPath = NSIndexPath(forRow: numberOfRows-1, inSection: (numberOfSections-1))
             self.itemsTable.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Bottom, animated: true)
         }
+        
+        var usedItems: [ItemObject] = dataManager.getNotCompletedItems()
+        
+        UIApplication.sharedApplication().applicationIconBadgeNumber = usedItems.count
         
     }
     
