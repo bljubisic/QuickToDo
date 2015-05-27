4a60d25b95825e1864e28b005e065fb435d112ae
bljubisic <bratislav.ljubisic@icloud.com>
2015-05-27 23:49:28 +0200

    Added invitation representation

---
+++ QuickToDo/InvitationObject.swift
@@ -0,0 +1,18 @@
+//
+//  InvitationObject.swift
+//  QuickToDo
+//
+//  Created by Bratislav Ljubisic on 5/26/15.
+//  Copyright (c) 2015 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+
+class InvitationObject: NSObject {
+    
+    var receiver: String = ""
+    var sender: String = ""
+    var confirmed: Int = 0
+    var sendername: String = ""
+    
+}
