4e007da859508a4a139942dcdae548cbca17cb0a
bljubisic <bratislav.ljubisic@icloud.com>
2015-05-27 23:49:06 +0200

    Added cordata object

---
+++ QuickToDo/Invitation.swift
@@ -0,0 +1,21 @@
+//
+//  Invitation.swift
+//  QuickToDo
+//
+//  Created by Bratislav Ljubisic on 5/26/15.
+//  Copyright (c) 2015 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Invitation)
+
+class Invitation: NSManagedObject {
+
+    @NSManaged var receiver: String
+    @NSManaged var sender: String
+    @NSManaged var confirmed: NSNumber
+    @NSManaged var sendername: String
+
+}
