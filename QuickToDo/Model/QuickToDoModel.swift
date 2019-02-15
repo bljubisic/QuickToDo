260d58f32f3d58637038d085f7c5fc3d123c24cd
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2019-02-15 23:58:39 +0100

    Get Hints implementation in Model

--- QuickToDo/Model/QuickToDoModel.swift
+++ QuickToDo/Model/QuickToDoModel.swift
@@ -10,10 +10,12 @@ import Foundation
 import RxSwift
 
 class QuickToDoModel: QuickToDoOutputs, QuickToDoInputs, QuickToDoProtocol {
+    
     private let itemsPrivate: PublishSubject<Item> = PublishSubject()
     private let cloudStatusPrivate: PublishSubject<CloudStatus> = PublishSubject()
+    private let itemHints: PublishSubject<String> = PublishSubject()
     
-    var items: Observable<Item>? {
+    var items: Observable<Item> {
         return itemsPrivate
     }
     
@@ -39,6 +41,16 @@ class QuickToDoModel: QuickToDoOutputs, QuickToDoInputs, QuickToDoProtocol {
         self.itemsPrivate.onNext(newItem)
         return(true, nil)
     }
+    
+    func getHints(for itemName: String) -> Observable<String> {
+        self.coreData.inputs.getHints(for: itemName) { (firstItem, secondItem) in
+            itemHints.onNext(firstItem.name)
+            itemHints.onNext(secondItem.name)
+            itemHints.onCompleted()
+        }
+        return itemHints.asObserver()
+    }
+    
     
     var inputs: QuickToDoInputs { return self }
     
